72b09a54a0d1744b324f19529d00ae3fca54abb3
nuhman <nuhman7@outlook.com>
2018-10-29 07:24:18 +0530

    add jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import renderer from 'react-test-renderer';
+
+
+
+describe('App Tests', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(<App/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
 });
